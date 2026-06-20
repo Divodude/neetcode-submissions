3a01f27bae6bc4627fc4462b9cc6061cc003c59c
Divyansh <155663331+Divodude@users.noreply.github.com>
2026-06-20 20:38:44 +0530

    Add: decode-ways - submission-4

---
+++ Data Structures & Algorithms/decode-ways/submission-4.py
@@ -0,0 +1,22 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        n=len(s)
+
+        if s[0]=="0":
+            return 0
+        dp={}
+        def rec(i):
+            if i ==n:
+                return 1
+            if s[i]=="0":
+                return 0 
+            if i in dp:
+                return dp[i]
+            pick_one=rec(i+1)
+            pick_two=0
+            if i+1<n and int(s[i:i+2])<=26:
+                pick_two=rec(i+2)
+            dp[i]=pick_one+pick_two
+            return dp[i]
+        return rec(0)
+        
