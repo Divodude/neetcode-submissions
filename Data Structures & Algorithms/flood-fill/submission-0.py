8d02cc0d6d9e8a5d44956bfdb66b7f0c18119a2e
Divyansh <155663331+Divodude@users.noreply.github.com>
2026-09-03 11:03:48 +0530

    Add: flood-fill - submission-0

---
+++ Data Structures & Algorithms/flood-fill/submission-0.py
@@ -0,0 +1,32 @@
+class Solution:
+    def floodFill(self, image, sr, sc, color):
+        old_color = image[sr][sc]
+
+       
+        if old_color == color:
+            return image
+
+        m = len(image)
+        n = len(image[0])
+
+        def dfs(r, c):
+            if r < 0 or r >= m or c < 0 or c >= n:
+                return
+
+            if image[r][c] != old_color:
+                return
+
+            image[r][c] = color
+
+          
+            dfs(r - 1, c)
+           
+            dfs(r + 1, c)
+           
+            dfs(r, c - 1)
+           
+            dfs(r, c + 1)
+
+        dfs(sr, sc)
+
+        return image
